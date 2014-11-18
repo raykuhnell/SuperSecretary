08b92a4704f2aa3ac67fdead3c7898e2f0888b57
Ray Kuhnell <raykuhnell@gmail.com>
2014-11-18 13:01:41 -0500

    Plugin System Unit Tests
    Added test handler and unit tests for assembly loading.

---
+++ SuperSecretary.Tests/HandlerManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperSecretary.Handlers;
+
+namespace SuperSecretary.Tests
+{
+    [TestClass]
+    public class HandlerManagerTests
+    {
+        [TestMethod]
+        [DeploymentItem("SuperSecretary.Tests.dll", "HandlerManagerTests")]
+        public void LoadAssembly()
+        {
+            var hm = HandlerManager.Instance;
+            hm.LoadAssembly(Environment.CurrentDirectory + "\\HandlerManagerTests\\SuperSecretary.Tests.dll");
+
+            Assert.IsTrue(hm.Handlers.ContainsKey("Test Handler"));
+
+            var testHandler = hm.Handlers["Test Handler"];
+            var result = testHandler.Do("", new HandlerOptions());
+            Assert.AreEqual("Test Value", result.Value, "Returned value is not correct.");
+        }
+    }
+}
---
+++ SuperSecretary.Tests/TestHandler.cs
@@ -0,0 +1,20 @@
+﻿using SuperSecretary.Handlers;
+
+namespace SuperSecretary.Tests
+{
+    public class TestHandler : IHandler
+    {
+        public string Name
+        {
+            get
+            {
+                return "Test Handler";
+            }
+        }
+
+        public HandlerResult Do(string fileName, HandlerOptions options)
+        {
+            return new HandlerResult() { Value = "Test Value" };
+        }
+    }
+}
